47168da23642982670acdedeceb9ebb7c6d482c2
Jakub Fronczyk <jakubfronczyk@gmail.com>
2022-10-05 14:08:29 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,32 +1,15 @@
 import React from 'react';
 import Header from './components/Header.js'
 import Meme from './components/Meme.js'
-// import logo from './images/meme-logo.png'
+
 
 function App() {
-
-
-  // PRACTICE
-  // function handleClick(){
-  //   alert('I was clicked')
-  // }
-
-  // function onImgOver(){
-  //   alert ('Ups you just mouse over on the image')
-  // }
 
   return (
     <>
       <Header />
       <Meme />
     </>
-    //  /* PRACTICE */
-    // <div className="container">
-    //   <img onMouseOver={onImgOver} src={logo} alt="logo" />
-    //   <button onClick={handleClick} >CLICK ME</button>
-    // </div>
-
-    
   );
 }
 
